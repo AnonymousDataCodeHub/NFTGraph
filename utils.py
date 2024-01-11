3dcb61d0a2ce7a5b9e862cf53cabc6dfafad3edd
AnonymousDataCodeHub <62274416+AnonymousDataCodeHub@users.noreply.github.com>
2024-01-11 21:42:50 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,12 +2,25 @@ import random
 from models.detector import *
 from dgl.data.utils import load_graphs
 import os
-from pygod.metrics import eval_precision_at_k,eval_recall_at_k,eval_roc_auc,eval_average_precision
-from pygod.models import DOMINANT,AnomalyDAE,DONE,AdONE,GAAN,GCNAE,CONAD,MLPAE
+from pygod.metric import eval_precision_at_k,eval_recall_at_k,eval_roc_auc,eval_average_precision
+from pygod.detector import *
 import torch.nn.functional
+from pyod.models.pca import PCA
+from pyod.models.ocsvm import OCSVM
+from pyod.models.lof import LOF
+from pyod.models.cblof import CBLOF
+from pyod.models.cof import COF
+from pyod.models.hbos import HBOS
+from pyod.models.sod import SOD
+from pyod.models.copod import COPOD
+from pyod.models.ecod import ECOD
+# from pyod.models.deep_svdd import DeepSVDD
+from pyod.models.loda import LODA
+from pyod.models.iforest import IForest
+
 
 class Dataset:
-    def __init__(self, name='tfinance', prefix='/datasets'):
+    def __init__(self, name='tfinance', prefix='datasets/'):
         graph = load_graphs(prefix + name)[0][0]
         self.name = name
         self.graph = graph
@@ -20,7 +33,20 @@ class Dataset:
         self.graph.ndata['test_mask'] = self.graph.ndata['test_masks'][:,trial_id]
         print(self.graph.ndata['train_mask'].sum(), self.graph.ndata['val_mask'].sum(), self.graph.ndata['test_mask'].sum())
 
-
+def toeval(labels, probs):
+    score = {}
+    with torch.no_grad():
+        if torch.is_tensor(labels):
+            labels = labels.cpu().numpy()
+        if torch.is_tensor(probs):
+            probs = probs.cpu().numpy()
+        score['AUROC'] = roc_auc_score(labels, probs)
+        score['AUPRC'] = average_precision_score(labels, probs)
+        labels = np.array(labels)
+        k = labels.sum()
+    score['RecK'] = sum(labels[probs.argsort()[-k:]]) / sum(labels)
+    return score
+    
 # supervised models
 model_detector_dict = {
     # Classic Methods
@@ -52,13 +78,35 @@ model_dict = {
     'CONAD': CONAD,
     'AnomalyDAE': AnomalyDAE,
     'AdONE': AdONE,
-    'GCNAE': GCNAE,
-    'MLPAE': MLPAE,
+    # 'GCNAE': GCNAE,
+    # 'MLPAE': MLPAE,
     'DOMINANT':DOMINANT,
-    'GAAN':GAAN
+    'GAAN':GAAN,
+    'SCAN':SCAN,
+    'Radar':Radar,
+    'ANOMALOUS':ANOMALOUS,
+    'ONE':ONE,
+    'OCGNN':OCGNN,
+    'CoLA':CoLA,
+    'GUIDE':GUIDE,
 }
 
 
+# PYOD models
+model_dict_od = {
+    'PCA': PCA,
+    'OCSVM': OCSVM,
+    'LOF': LOF,
+    'CBLOF': CBLOF,
+    'COF':COF,
+    'HBOS':HBOS,
+    'SOD':SOD,
+    'COPOD':COPOD,
+    'ECOD':ECOD,
+    'ONE':ONE,
+    'LODA':LODA,
+    'IForest':IForest,
+}
 def save_results(results, file_id):
     if not os.path.exists('results/'):
         os.mkdir('results/')
@@ -242,95 +290,121 @@ activations = {
     'LeakyReLU': torch.nn.functional.leaky_relu,
     'Tanh': torch.nn.functional.tanh}
 
-param_space['DONE'] = {
-    'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'num_layers': [2, 3, 4],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
 
-param_space['AdONE'] = {
+for unsupervised_model in model_dict.keys():
+    param_space[unsupervised_model] = {
     'hid_dim': [16, 32, 64],
     'dropout': [0, 0.1, 0.2, 0.3],
     'num_layers': [2, 3, 4],
     'lr': 10 ** np.linspace(-3, -1, 1000),
     'weight_decay':[1e-5,1e-4,1e-3],
     'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
-param_space['AnomalyDAE'] = {
+    'gamma':[0.01,0.1,0.5,1],
     'embed_dim': [16, 32, 64],
-    'out_dim': [4,8,16],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'num_layers': [2, 3, 4],
-    'weight_decay': [1e-5,1e-4,1e-3],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
-
-param_space['CONAD'] = {
-    'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'num_layers': [2, 3, 4],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'weight_decay': [1e-5,1e-4,1e-3],
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
-param_space['DOMINANT'] = {
-    'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'num_layers': [2, 3, 4],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'weight_decay': [1e-5,1e-4,1e-3],
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
-param_space['GAAN'] = {
     'noise_dim': [8, 16, 32, 64],
-    'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'generator_layers': [2, 3, 4],
-    'encoder_layers': [2, 3, 4],
-    'weight_decay': [1e-5,1e-4,1e-3],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
+    'hid_a': [16, 32, 64],
+    'hid_s': [16, 32, 64],
+    'alpha':[0.01,0.1,0.5,1],
+    'theta':[0.1,1,10],
+    'eta':[0.1,1,10],
+    'weight':[0.01,0.1,0.5,1],
+    'w1':[0.1,0.2,0.5,0.7,1],
+    'w2':[0.1,0.2,0.5,0.7,1],
+    'w3':[0.1,0.2,0.5,0.7,1],
+    'w4':[0.1,0.2,0.5,0.7,1],
+    'w5':[0.1,0.2,0.5,0.7,1],
+    'graphlet_size':[4,10,50],
+    'beta':[0.01,0.1,0.5,1],
+    'eps':[0.1,0.5,0.7],
+    'mu':[2,10,20],
+    }
+    
+    
 
-param_space['GCNAE'] = {
+for unsupervised_od_model in model_dict_od.keys():
+    param_space[unsupervised_od_model] = {
     'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
+    'dropout': [0.0, 0.1, 0.2, 0.3],
     'num_layers': [2, 3, 4],
     'lr': 10 ** np.linspace(-3, -1, 1000),
     'weight_decay':[1e-5,1e-4,1e-3],
     'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
-
-param_space['MLPAE'] = {
-    'hid_dim': [16, 32, 64],
-    'dropout': [0, 0.1, 0.2, 0.3],
-    'num_layers': [2, 3, 4],
-    'lr': 10 ** np.linspace(-3, -1, 1000),
-    'weight_decay': [1e-5,1e-4,1e-3],
-    'act': [activations['ReLU'], activations['LeakyReLU'], activations['Tanh']],
-}
-
+    'kernel':['linear', 'poly', 'rbf', 'sigmoid'],
+    'nu':[0.2,0.5,0.7,1],
+    'degree':[2,3],
+    'gamma':'auto',
+    'coef0':[0.0,0.5,1],
+    'tol':[1e-3,1e-2],
+    'algorithm' : ['auto', 'ball_tree', 'kd_tree', 'brute'],
+    'n_neighbors':[10,20,30],
+    'leaf_size':[20,30,50],
+    'p':[1,2,3],
+    'n_clusters':[5,8,10,20],
+    'alpha':[0.6,0.8,0.9],
+    # 'beta':[2,5,10],
+    'n_bins':[5,10,20],
+    'alpha':[0.01,0.1,0.5,1],
+    'tol':[1e-3,1e-2,0.5],
+    'hid_a': [16, 32, 64],
+    'hid_s': [16, 32, 64],
+    'gamma':[0.01,0.1,0.5,1],
+    'n_random_cuts':[10,100],
+    'n_estimators':[20,50,100],
+    'max_samples':['auto',50,100],    
+    }
 
 best_param = {}
 
 def set_best_param(model, dataset,t):
     model_config = {'model': model, 'lr': 0.01, 'drop_rate': 0}
-    
-    if not model in model_detector_dict.keys():
-        for k, v in best_param[model].items():
+    if model in model_dict_od.keys(): # using default hyperparameters
+        return model_config
+    if dataset.endswith('_remove'):
+        for k, v in best_param_remove[model].items():
             model_config[k] = v
+    else:
+        for k, v in best_param[model].items():
+            model_config[k] = v        
 
     return model_config
 
 
+
+best_param['MLP'] = {'model': 'MLP', 'lr': 0.0019692202554791715, 'drop_rate': 0.2, 'h_feats': 16, 'num_layers': 1, 'activation': 'LeakyReLU', 'in_feats': 8}
+best_param['KNN'] = {'model': 'KNN', 'lr': 0.01, 'drop_rate': 0, 'k': 40, 'weights': 'uniform', 'p': 2, 'in_feats': 8}
+best_param['SVM'] = {'model': 'SVM', 'lr': 0.01, 'drop_rate': 0, 'weights': 'distance', 'C': 2.6880010215376062, 'in_feats': 8}
+best_param['RF'] = {'model': 'RF', 'lr': 0.01, 'drop_rate': 0, 'n_estimators': 129, 'criterion': 'entropy', 'max_samples': 0.7144144144144144, 'in_feats': 8}
+best_param['GCN'] = {'model': 'GCN', 'lr': 0.004174655289253135, 'drop_rate': 0, 'h_feats': 32, 'num_layers': 1, 'activation': 'ReLU', 'in_feats': 8}
+best_param['SGC'] = {'model': 'SGC', 'lr': 0.031586354082678174, 'drop_rate': 0.2, 'h_feats': 16, 'k': 2, 'mlp_layers': 1, 'in_feats': 8}
+best_param['GIN'] = {'model': 'GIN', 'lr': 0.028018665564591955, 'drop_rate': 0.2, 'h_feats': 32, 'num_layers': 3, 'agg': 'max', 'activation': 'Tanh', 'in_feats': 8}
+best_param['GraphSAGE'] = {'model': 'GraphSAGE', 'lr': 0.0036354699612933176, 'drop_rate': 0.2, 'h_feats': 32, 'num_layers': 1, 'agg': 'pool', 'activation': 'ReLU', 'in_feats': 8}
+best_param['GAT'] = {'model': 'GAT', 'lr': 0.03115254223555488, 'drop_rate': 0.2, 'h_feats': 16, 'num_heads': 2, 'num_layers': 3, 'in_feats': 8}
+best_param['GT'] = {'model': 'GT', 'lr': 0.08431909292866255, 'drop_rate': 0, 'h_feats': 16, 'num_heads': 2, 'num_layers': 2, 'in_feats': 8}
+best_param['GAS'] = {'model': 'GAS', 'lr': 0.08994022174092044, 'drop_rate': 0, 'h_feats': 32, 'num_layers': 1, 'k': 14, 'dist': 'cosine', 'activation': 'Tanh', 'in_feats': 8, 'mlp_layers': 0}
+best_param['BernNet'] = {'model': 'BernNet', 'lr': 0.037288213071828336, 'drop_rate': 0.1, 'h_feats': 16, 'mlp_layers': 2, 'orders': 5, 'activation': 'Tanh', 'in_feats': 8}
+best_param['AMNet'] = {'model': 'AMNet', 'lr': 0.05620173848083188, 'drop_rate': 0, 'h_feats': 64, 'num_layers': 2, 'orders': 2, 'activation': 'LeakyReLU', 'in_feats': 8}
+best_param['GHRN'] = {'model': 'GHRN', 'lr': 0.06604193962330306, 'drop_rate': 0, 'h_feats': 16, 'del_ratio': 0.05939826693920356, 'num_layers': 2, 'mlp_layers': 2, 'in_feats': 8}
+best_param['GATSep'] = {'model': 'GATSep', 'lr': 0.04916903577628029, 'drop_rate': 0, 'h_feats': 16, 'num_heads': 2, 'num_layers': 1, 'in_feats': 8}
+best_param['PCGNN'] = {'model': 'PCGNN', 'lr': 0.023625084654779464, 'drop_rate': 0, 'h_feats': 32, 'del_ratio': 0.09935935935935936, 'add_ratio': 0.06377377377377377, 'dist': 'cosine', 'num_layers': 2, 'in_feats': 8}
+
+
+best_param_remove = {}
+best_param_remove['MLP'] = {'model': 'MLP', 'lr': 0.0046630349297427315, 'drop_rate': 0.3, 'h_feats': 16, 'num_layers': 4, 'activation': 'ReLU', 'in_feats': 8}
+best_param_remove['KNN'] = {'model': 'KNN', 'lr': 0.01, 'drop_rate': 0, 'k': 49, 'weights': 'distance', 'p': 2, 'in_feats': 8}
+best_param_remove['SVM'] = {'model': 'SVM', 'lr': 0.01, 'drop_rate': 0, 'weights': 'distance', 'C': 1.596626022101426, 'in_feats': 8}
+best_param_remove['RF'] = {'model': 'RF', 'lr': 0.01, 'drop_rate': 0, 'n_estimators': 158, 'criterion': 'entropy', 'max_samples': 0.13693693693693693, 'in_feats': 8}
+best_param_remove['GCN'] = {'model': 'GCN', 'lr': 0.0019692202554791715, 'drop_rate': 0.2, 'h_feats': 16, 'num_layers': 1, 'activation': 'LeakyReLU', 'in_feats': 8}
+best_param_remove['SGC'] = {'model': 'SGC', 'lr': 0.04051423171114647, 'drop_rate': 0.2, 'h_feats': 16, 'k': 1, 'mlp_layers': 1, 'in_feats': 8}
+best_param_remove['GIN'] = {'model': 'GIN', 'lr': 0.005137013543351339, 'drop_rate': 0.1, 'h_feats': 16, 'num_layers': 1, 'agg': 'mean', 'activation': 'LeakyReLU', 'in_feats': 8}
+best_param_remove['GraphSAGE'] = {'model': 'GraphSAGE', 'lr': 0.003503842245290676, 'drop_rate': 0, 'h_feats': 16, 'num_layers': 2, 'agg': 'mean', 'activation': 'Tanh', 'in_feats': 8}
+best_param_remove['GAT'] = {'model': 'GAT', 'lr': 0.05031548945038054, 'drop_rate': 0.3, 'h_feats': 16, 'num_heads': 2, 'num_layers': 3, 'in_feats': 8}
+best_param_remove['GT'] = {'model': 'GT', 'lr': 0.012738113231864785, 'drop_rate': 0.1, 'h_feats': 16, 'num_heads': 2, 'num_layers': 3, 'in_feats': 8}
+best_param_remove['GAS'] = {'model': 'GAS', 'lr': 0.01783410220710008, 'drop_rate': 0, 'h_feats': 64, 'num_layers': 1, 'k': 32, 'dist': 'cosine', 'activation': 'LeakyReLU', 'in_feats': 8, 'mlp_layers': 0}
+best_param_remove['BernNet'] = {'model': 'BernNet', 'lr': 0.014831025143361045, 'drop_rate': 0.1, 'h_feats': 64, 'mlp_layers': 2, 'orders': 2, 'activation': 'Tanh', 'in_feats': 8}
+best_param_remove['AMNet'] = {'model': 'AMNet', 'lr': 0.09418331534647952, 'drop_rate': 0.1, 'h_feats': 32, 'num_layers': 1, 'orders': 2, 'activation': 'ReLU', 'in_feats': 8}
+best_param_remove['GHRN'] = {'model': 'GHRN', 'lr': 0.07479522515621821, 'drop_rate': 0.2, 'h_feats': 16, 'del_ratio': 0.013936192742241428, 'num_layers': 3, 'mlp_layers': 2, 'in_feats': 8}
+best_param_remove['GATSep'] = {'model': 'GATSep', 'lr': 0.04567301270168744, 'drop_rate': 0.2, 'h_feats': 16, 'num_heads': 2, 'num_layers': 3, 'in_feats': 8}
+best_param_remove['PCGNN'] = {'model': 'PCGNN', 'lr': 0.0017711210643450886, 'drop_rate': 0.1, 'h_feats': 16, 'del_ratio': 0.04874874874874875, 'add_ratio': 0.15787787787787788, 'dist': 'cosine', 'num_layers': 2, 'in_feats': 8}
 
 best_param['GCNAE'] = {
     'hid_dim': 16,
